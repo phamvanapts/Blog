cc7c845f114d42b40c200e76bd21b40077326253
phamvana <phamvana@gmail.com>
2024-04-10 22:51:25 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 /**
+ * Chapter 2: Introduction to npm & Express
+ * Introduction to express
  * Khi chạy chương trình chạy file này đầu tiên
- * 1. cập nhật express vào ứng dụng
- * 2. đặt tên biến khởi tạo
- * 3. đặt cổng 4000
+ * Thêm thư viên express vào ứng dụng
+ * Đây là thư viện chạy suốt chương trình của chúng ta
+ * Người tạo: Phạm Văn Á
+ * Ngày tạo: 10/4/2024
  */
 
 const express = require('express');
+
+/**
+ * Phần tự bổ sung
+ * Thư viện này theo dõi quá trình đưa dữ liệu từ client lên server
+ * quá trình xử lý có thành công hay không
+ * Người tạo: Phạm Văn Á
+ * Ngày tạo: 10/4/2024
+ */
 const morgan = require('morgan');
+
 const path = require('path');
+
+/**
+ * Phần tự bổ sung
+ * Thư viện màu của console
+ * Chỉ để trang trí khi chạy console
+ * Người tạo: Phạm Văn Á
+ * Ngày tạo: 10/4/2024
+ */
 const colors = require('colors');
 
 // import dotenv from "dotenv"; không sử dụng nữa
@@ -17,16 +37,60 @@ const bodyParser = require('body-parser');
 
 // import connectDB from "./db/db.js";
 const connectDB = require('./db/db.js');
-//import Model
-// import BlogPost from "./models/BlogPost.js";
+
+/**
+ * import Model
+ * Lấy dữ liệu từ /models/BlogPost.js
+ * Định nghĩa cấu trúc cần lưu trữ dữ liệu kết nối trong MongoDB
+ * Phạm Văn Á
+ * 10/4/2024
+ */
 const BlogPost = require('./models/BlogPost.js');
+/**
+ * Chapter 2: Introduction to npm & Express
+ * Introduction to express
+ * Đầu tiên, thêm express vào ứng dụng
+ * Đặt tên ứng dụng pva
+ * Sử dụng express để chạy toàn bộ ứng dụng
+ * Phạm Văn Á
+ * 10/4/2024
+ */
 const pva = new express();
 
+ /**
+  * Chapter 4: Template Engines
+  * npm install ejs --save
+  * Render trang HTML động
+  * Sử dụng EJS vào ứng dụng
+  * Với pva.set('view engine','ejs') chúng ta thông báo cho Express sử dụng EJS
+  * Phạm Văn Á
+  * 10/4/2024
+  */
 const ejs = require('ejs');
 pva.set('view engine','ejs');
-//public folder for serving static files
+
+/**
+ * Chapter 2: Introduction to npm & Express
+ * public folder for serving static files
+ * Sử dụng chạy các trang tĩnh ví dụ như about.html
+ * phần này sẽ được thay thế bởi ejs
+ * Phạm Văn Á
+ * 10//4/2024
+ */
 pva.use(express.static('public'));
+
+/**
+ * Phần tự bổ sung
+ * Sử dụng để ghi nhật ký cho các req và req
+ * Người tạo: Phạm Văn Á
+ * Ngày tạo: 10/4/2024
+ * 
+ */
 pva.use(morgan('dev'));
+
+/**
+ * 
+ */
 pva.use(bodyParser.json());
 pva.use(bodyParser.urlencoded({extended:true}));
 connectDB();
@@ -34,7 +98,11 @@ connectDB();
 pva.get('/',async(req,res)=>{
     const blogposts = await BlogPost.find({});
     // res.sendFile(path.resolve(__dirname,'/index.html')); ==> chuyển đến trang tĩnh
-    res.render('index', {blogposts:blogposts});
+    res.render('index', 
+    {
+        blogposts:blogposts
+    }
+);
     // console.log(blogposts);
 });
 //about
@@ -77,11 +145,29 @@ pva.post('/posts/store', async(req,res)=>{
         res.redirect('/');
 });
     
-    // res.sendFile(path.resolve(__dirname,'public/post.html'));
 
-
-
+/**
+ * Phần tự bổ sung
+ * Đặt tên biến PORT là chạy ứng dụng
+ * Sử dụng dotenv - biến môi trường để cài đặt cổng
+ * Có thể thay đổi cổng trong file .env hoặc giá trị gán tại file này
+ * Mục đích nếu file .env có lỗi hoặc không tải được thì vẫn chạy được server
+ * Người tạo: Phạm Văn Á
+ * Ngày tạo: 10/4/2024
+ */
 const PORT = process.env.PORT || 4000;
+
+/**
+ * Chapter 1: Introduction
+ * Chapter 2: Introduction to npm & Express
+ * Ứng dụng hoạt động tại cổng PORT mà ta cài đặt
+ * Bên cạnh đó có thể ghi các thông báo ra màn hình console
+ * Xoá các thông tin trước đó khi chạy chương trình bằng lệnh
+ * consle.clear tự tìm hiểu
+ * Các thông tin khác chỉ ghi cho vui, hấp dẫn khi thực hiện chương trình
+ * Người tạo: Phạm Văn Á
+ * Ngày tạo: 10/4/2024
+ */
 pva.listen(PORT, ()=>{
     console.clear();
     console.log(`================================`);
