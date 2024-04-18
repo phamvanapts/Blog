b7be5b612df50fe4f84a8e4578e0b3a4e50ebd91
phamvana <phamvana@gmail.com>
2024-04-18 22:45:12 +0700

    chapter 14

--- controllers/newPostController.js
+++ controllers/newPostController.js
@@ -5,7 +5,9 @@
  */
 module.exports = (req,res) =>{
     if(req.session.userId){
-       return res.render('create');
+       return res.render('create',{
+        createPost: true
+       });
     }
     res.redirect('/auth/login');
 }
